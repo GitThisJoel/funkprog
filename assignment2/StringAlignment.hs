e392e29c0bb08fc6459b65d3f31ddf4473ebb67f
winkelmannvictor <Victor.Winkelmann@hotmail.com>
2020-05-04 18:03:56 +0200

    klar med 2c

--- assignment2/StringAlignment.hs
+++ assignment2/StringAlignment.hs
@@ -5,36 +5,40 @@
 -- Victor Winkelmann (and vi6253wi-s) --------------------------------------- --
 -- -------------------------------------------------------------------------- --
 
+module StringAlignment where 
+import Data.Char
+
 type AlignmentType = (String,String)
 
 scoreMatch = 0
-scoreMismatch = 0
-scoreSpace = 0
+scoreMismatch = -1
+scoreSpace = -1
+string1 = "writers"
+string2 = "vintner"
 
 -- Input: Two strings s and t, and values for scoreMatch, scoreMismatch, and scoreSpace.
 -- Output: All optimal alignments between s and t.
 optimalAlignments :: Int -> Int -> Int -> String -> String -> [AlignmentType]
-optimalAlignments scoreMa scoreMi scoreSp s t = do
-    let scoreMatch = scoreMa
-    let scoreMismatch = scoreMi
-    let scoreSpace = scoreSp
-    similarityScore s t 
-    ("hej", "hej")
-
+optimalAlignments scoreMa scoreMi scoreSp s t = []
 
 -- Helper functions to similarityScore
-sim :: [a] -> [a] -> Int
-sim (x:xs) (y:ys) = max [sim xs ys + score x y, 
-                          sim xs (y:ys) + score x '-', 
-                          sim (x:xs) ys + score '-' y]
+sim :: [Char] -> [Char] -> Int
+sim [] _ = 0
+sim _ [] = 0
+sim (x:xs) (y:ys) = maximum [sim xs ys + (score x y), 
+                             sim xs (y:ys) + (score x '-'), 
+                             sim (x:xs) ys + (score '-' y)]
+
 
 -- sim ((x:xs),(y:ys)) = sim (xs,ys) + score (x,y)
-score :: a -> a -> Int
+score :: Char -> Char -> Int
+score '-' _ = scoreSpace
+score _ '-' = scoreSpace
 score c1 c2 
-    | c1 == '-' = scoreSpace
-    | c2 == '-' = scoreSpace
     | c1 == c2 = scoreMatch
     | otherwise = scoreMismatch
+
+
 
 -- returns the score of the optimal alignment of the two strings
 similarityScore :: String -> String -> Int
@@ -42,14 +46,18 @@ similarityScore string1 string2 = sim string1 string2
 
 
 -- What does this function do?
+-- The function takes two heads and a list with tuples, the elements in the tupels 
+-- are also lists. For all elements in the list it adds the first head to the first 
+-- element in the tuple as a new head to that list. It also does the same for the 
+-- second element in all the tuples but with the second head. 
 attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
 attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
 
 
 -- returns all maximum values in a list given by a function
 maximaBy :: Ord b => (a -> b) -> [a] -> [a]
-maximaBy _ _ = []
--- maximaBy valueFcn xs
+maximaBy valueFcn xs = [a | a <- xs, valueFcn a == maxList] where
+    maxList = maximum $ map valueFcn xs
 
 
 -- returns a list of all optimal alignments between string1 and string2
@@ -60,5 +68,4 @@ optAlignments _ _ = []
 
 -- should output to the screen in a readable fashion
 -- outputOptAlignments string1 string2
-
 -------------------------------------------------------------------
