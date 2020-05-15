1de848ae69928ed7cb96fe325e1d39b2715db64c
GitThisJoel <43029437+GitThisJoel@users.noreply.github.com>
2020-05-15 15:35:06 +0200

    tjena fittan, klar med 'chars'

--- assignment3/Parser.hs
+++ assignment3/Parser.hs
@@ -35,7 +35,9 @@ word :: Parser String
 word = token (letter # iter letter >-> cons)
 
 chars :: Int -> Parser String
-chars n =  error "chars not implemented"
+chars n
+  | n == 0 = return []
+  | otherwise = char # chars (n-1) >-> cons
 
 accept :: String -> Parser String
 accept w = (token (chars (length w))) ? (==w)
